66de9bdbc1536a46dd091621bb2545d8d312ab94
rama <rama.alshaban@gmail.com>
2022-11-18 17:35:10 +0300

    setting socket.io

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const path = require('path');
+const http = require('http');
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 
+const server = http.createServer(app);
+const io = socket(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Run when client connects
+io.on('connection', socket =>{
+    console.log('New WS Connection...');
+});
 const PORT = 3006 || process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
