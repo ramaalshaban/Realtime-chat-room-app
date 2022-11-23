7b63926114d087fcf9d32a7936d35763ab10fcf8
rama <rama.alshaban@gmail.com>
2022-11-23 21:13:10 +0300

    add user info to the dom

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const formatMessage = require("./utils/messages");
-const { userJoin, getCurrentUser } = require("./utils/users");
+const { userJoin, getCurrentUser ,userLeave,getRoomUsers} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
@@ -34,7 +34,7 @@ io.on("connection", (socket) => {
         "message",
         formatMessage(botName, `${user.username} has joined the chat`)
       );
-
+    //Listen for chatMessage
     socket.on("chatMessage", (msg) => {
       const user = getCurrentUser(socket.id);
 
@@ -44,7 +44,12 @@ io.on("connection", (socket) => {
 
     //Runs when a client disconnect
     socket.on("disconnect", () => {
-    io.emit("message", formatMessage(botName, `A user has left the chat`));
+
+        const user = userLeave(socket.id);
+        if(user){
+            io.to(user.room).emit("message", formatMessage(botName, `${user.username} has left the chat`));
+
+        }
   });
 });
 
