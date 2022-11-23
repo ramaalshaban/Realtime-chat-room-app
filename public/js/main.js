fe535b1eee4e81a656c16b137c7451b9c879dfa8
rama <rama.alshaban@gmail.com>
2022-11-23 12:00:13 +0300

    Display the message

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,15 @@
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
 // Message from server
 socket.on('message', (message) => {
   console.log(message);
+  outputMessage(message);
+
+  //Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 // create an event listener for the submission of the form
@@ -16,4 +21,22 @@ chatForm.addEventListener('submit', (e) => {
   const msg = e.target.elements.msg.value;
   // Emmit the message to server
   socket.emit('chatMessage',msg);
+
+  //Clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+
 });
+
+
+//Output message from DOM
+function outputMessage(message){
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class= "meta"> Brad <span> now pm</span> </p>
+  <p class="test">
+  ${message}
+  </p>`
+// we dont have id here so we want  to collect the class
+  document.querySelector('.chat-messages').appendChild(div);  
+}
